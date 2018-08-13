45c19703c79912838e753394f02056f1ac72429c
andy-shi88 <shi88.andy@gmail.com>
2018-08-13 20:17:12 +0800

    migration configurations

---
+++ migrations/versions/89667820f7f6_create_kraepelin_table.py
@@ -0,0 +1,28 @@
+"""create-kraepelin-table
+
+Revision ID: 89667820f7f6
+Revises: 
+Create Date: 2018-08-13 18:47:20.783749
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '89667820f7f6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('kraepelins',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('answers', sa.Text),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime)
+        )
+
+def downgrade():
+    op.drop_table('kraepelins')
