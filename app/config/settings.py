bafd9d71d178ed0e37e6184fc7587714832a0a16
andy-shi88 <shi88.andy@gmail.com>
2018-08-13 20:16:29 +0800

    initialize database and ORM config

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,4 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get('DEBUG')
+
+# SQLAlchemy and Flask-migrate configuration
+SQLALCHEMY_DATABASE_URI='mysql://' + os.environ.get('DB_USER') + ':' + os.environ.get('DB_PASSWORD') + '@' + os.environ.get('DB_HOST') + '/' + os.environ.get('DB_NAME')
