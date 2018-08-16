852bf0eae762e05e24d14980f4b940594eee04af
andy-shi88 <shi88.andy@gmail.com>
2018-08-16 11:38:12 +0800

    fix logging configuration

--- app/kraepelin.py
+++ app/kraepelin.py
@@ -9,30 +9,13 @@ from .routes.public import public_routes
 from .config import settings
 from .models import db
 
-logging.config.dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': os.getenv('LOGGING_LEVEL_ROOT', 'INFO'),
-        'handlers': ['wsgi']
-    }
-})
-
-
 def create_app(configuration):
-    log = logging.getLogger(__name__)
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
     app.config.from_object(configuration)
     # init db
     db.init_app(app)
     # logging_tree.printout()
-    log.info('Starting Flask app %s...', __name__)
+    app.logger.info('Starting Flask app %s...', __name__)
     app.wsgi_app = ProxyFix(app.wsgi_app)
     # set configuration
     app.config.from_object(configuration)
