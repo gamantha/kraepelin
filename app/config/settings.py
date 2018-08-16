a2755631b5ca1cd6039c7f37d6766a9df5d8cc9a
andy-shi88 <shi88.andy@gmail.com>
2018-08-16 11:36:27 +0800

    set sqlalchemy tracking

--- app/config/settings.py
+++ app/config/settings.py
@@ -8,3 +8,4 @@ DEBUG = os.environ.get('DEBUG')
 
 # SQLAlchemy and Flask-migrate configuration
 SQLALCHEMY_DATABASE_URI='mysql://' + os.environ.get('DB_USER') + ':' + os.environ.get('DB_PASSWORD') + '@' + os.environ.get('DB_HOST') + '/' + os.environ.get('DB_NAME')
+SQLALCHEMY_TRACK_MODIFICATIONS=False
