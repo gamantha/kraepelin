9e3e757ca22adedf74c60861977952b78ae64b0f
andy-shi88 <shi88.andy@gmail.com>
2018-08-22 16:10:08 +0800

    add route for result

--- app/routes/public.py
+++ app/routes/public.py
@@ -13,7 +13,8 @@ def assess_kraepelin_test():
     # assess user kraepelin test result
     return public_controller.assess_result(request)
 
-@public_routes.route('/assess_result', methods=['GET'])
-def assess_kraepelin_result():
+@public_routes.route('/assess_result/<id>', methods=['GET'])
+def assess_kraepelin_result(id):
     # result page
-    return render_template('result.html', title='Result')
+    graph_data = public_controller.get_filled_answer(id)
+    return render_template('result.html', title='Result', result_id=id, graph_data=graph_data)
