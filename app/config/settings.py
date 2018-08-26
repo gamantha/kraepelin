d26d9034ced6f8f38396ce22b64cf1e9deae84c7
andy-shi88 <shi88.andy@gmail.com>
2018-08-26 14:24:13 +0800

    add secret env

--- app/config/settings.py
+++ app/config/settings.py
@@ -6,6 +6,8 @@ load_dotenv('.env')
 
 DEBUG = os.environ.get('DEBUG')
 
+SECRET_KEY=os.environ.get('SESSION_SECRET')
+
 # SQLAlchemy and Flask-migrate configuration
 SQLALCHEMY_DATABASE_URI='mysql://' + os.environ.get('DB_USER') + ':' + os.environ.get('DB_PASSWORD') + '@' + os.environ.get('DB_HOST') + '/' + os.environ.get('DB_NAME')
 SQLALCHEMY_TRACK_MODIFICATIONS=False
