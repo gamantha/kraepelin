2988d07b530e4701c1f5c08f4090984c79e0b4ae
andy-shi88 <shi88.andy@gmail.com>
2018-08-26 14:24:37 +0800

    add simple user login handler

--- app/routes/public.py
+++ app/routes/public.py
@@ -1,20 +1,41 @@
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint, request, render_template, redirect, url_for, session, flash
 from ..controllers import public_controller
+from ..util.login_required import login_required
+
 public_routes = Blueprint('public', __name__)
 
+@public_routes.route('/', methods=['GET'])
+def login_page():
+    # get login page
+    return render_template('login.html', title='Login')
+
+@public_routes.route('/login', methods=['POST'])
+def post_login():
+    # post login information
+    result = public_controller.get_user(request)
+    if result is None:
+        flash('email yang anda masukkan salah atau belum terdaftar.')
+        return redirect(url_for('public.login_page'))
+    session['user_id'] = result['id']
+    return redirect(url_for('public.execute_kraepelin_test'))
+
+
 @public_routes.route('/test', methods=['GET'])
-def execute_kraepelin_test():
+@login_required
+def execute_kraepelin_test(*args, **kwargs):
     # generate random value n*n
     data = public_controller.index(request)
     return render_template('index.html', title='Home', data=data)
 
 @public_routes.route('/assess', methods=['POST'])
-def assess_kraepelin_test():
+@login_required
+def assess_kraepelin_test(*args, **kwargs):
     # assess user kraepelin test result
-    return public_controller.assess_result(request)
+    return public_controller.assess_result(request, kwargs['user_id'])
 
 @public_routes.route('/assess_result/<id>', methods=['GET'])
-def assess_kraepelin_result(id):
+@login_required
+def assess_kraepelin_result(id, *args, **kwargs):
     # result page
     graph_data = public_controller.get_filled_answer(id)
     return render_template('result.html', title='Result', result_id=id, graph_data=graph_data)
