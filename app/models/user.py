4bea5d57e1bceba993c2708e93c8c55f2c76252d
andy-shi88 <shi88.andy@gmail.com>
2018-08-27 11:48:05 +0800

    add quota column to user

--- app/models/user.py
+++ app/models/user.py
@@ -11,6 +11,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255))
+    quota = db.Column(db.Integer, default=0)
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
 
