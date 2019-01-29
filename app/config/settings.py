41cbdbfbedf4e94c1039931ea7f1b63ffead42b3
andy-shi88 <shi88.andy@gmail.com>
2019-01-29 21:04:46 +0800

    add sqlalchemy bind for multiple db

--- app/config/settings.py
+++ app/config/settings.py
@@ -11,3 +11,6 @@ SECRET_KEY=os.environ.get('SESSION_SECRET')
 # SQLAlchemy and Flask-migrate configuration
 SQLALCHEMY_DATABASE_URI='mysql://' + os.environ.get('DB_USER') + ':' + os.environ.get('DB_PASSWORD') + '@' + os.environ.get('DB_HOST') + '/' + os.environ.get('DB_NAME')
 SQLALCHEMY_TRACK_MODIFICATIONS=False
+SQLALCHEMY_BINDS = {
+  'user': 'mysql://' + os.environ.get('DB_USER') + ':' + os.environ.get('DB_PASSWORD') + '@' + os.environ.get('DB_HOST') + '/' + os.environ.get('DB_CHAMILO'),
+}
